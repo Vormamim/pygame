20573bba46f5dc446ecf52e2af9c35c7efecfd8b
Vormamim <dean.groom@det.nsw.edu.au>
2024-11-03 20:20:45 +1100

    setup game window

---
+++ setup.py
@@ -0,0 +1,9 @@
+import pygame
+import random
+
+#create a window 800x600
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("My Game")
+#create a clock
+clock = pygame.time.Clock()
